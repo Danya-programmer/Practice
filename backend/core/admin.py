6b3f260d155a5843fd25023ac3e6d54e62b02962
Daniil <danbgavindv90835@gmail.com>
2025-12-02 23:20:37 +0300

    fix: register models to admin

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import JobCategory, Vacancy, Employer, Area
 # Register your models here.
+
+admin.site.register(JobCategory)
+admin.site.register(Vacancy)
+admin.site.register(Employer)
+admin.site.register(Area)
